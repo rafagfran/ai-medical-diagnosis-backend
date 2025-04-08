d3ef5f61432ccddc95cff64bdf3ce262b771bce6
Rafael Gonçalves <rafaelgfra@hotmail.com>
2025-04-08 16:06:33 -0300

    feat: create table exams_diagnostic

---
+++ mediai/src/main/resources/db/migration/V6__create-table-exams_diagnostic.sql
@@ -0,0 +1,9 @@
+CREATE TABLE exams_diagnostic (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY ,
+    exam_result_id UUID NOT NULL,
+    possible_illness VARCHAR(150) NOT NULL,
+    probability FLOAT NOT NULL ,
+    description VARCHAR(150) NOT NULL,
+    medical_validation BOOLEAN DEFAULT false,
+    FOREIGN KEY (exam_result_id) REFERENCES exam_results(id)
+);
