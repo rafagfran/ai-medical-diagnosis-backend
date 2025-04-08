e988aea6ac9e90dfee25bbc99ed221e357eaec9d
Rafael Gonçalves <rafaelgfra@hotmail.com>
2025-04-08 16:06:14 -0300

    feat: create table exams_result

---
+++ mediai/src/main/resources/db/migration/V5__create-table-exams_result.sql
@@ -0,0 +1,9 @@
+CREATE TABLE exams_result (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY ,
+    exam_id UUID NOT NULL,
+    indicator_id UUID NOT NULL,
+    value VARCHAR(100) NOT NULL,
+    status VARCHAR(100) NOT NULL ,
+    FOREIGN KEY (exam_id) REFERENCES exams(id) ON DELETE CASCADE,
+    FOREIGN KEY (indicator_id) REFERENCES indicator(id)
+);
