7a7a406dd2e9c02d25518addbd7545deb5460151
Rafael Gonçalves <rafaelgfra@hotmail.com>
2025-04-08 15:07:55 -0300

    feat: initial commit

---
+++ mediai/src/main/java/com/mediai/MediAiApplication.java
@@ -0,0 +1,13 @@
+package com.mediai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediAiApplication.class, args);
+	}
+
+}
---
+++ mediai/src/test/java/com/mediai/MediAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
